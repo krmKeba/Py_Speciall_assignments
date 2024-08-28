6a983bcf82e1077038b92d760e0a9f525abcab33
“[Kebafilwe <[krmfilwe@gmail.com]>
2024-08-28 19:51:37 +0200

    assignment_week_1

---
+++ user_input.py
@@ -0,0 +1,9 @@
+name = input("Enter your Name: ")
+age = input("Enter your Age: ")
+location = input("Enter you Location: ")
+pers_mssg = f"Hello {name}, you are {age} years old and live in {location}."
+
+print(name)
+print(age)
+print(location)
+print(pers_mssg)
